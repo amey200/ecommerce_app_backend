3e6fb6d56943e160d8466ab5b2122c6034600c0f
amey2027 <amey.vagare03.gmail.com>
2026-07-24 20:20:34 +0530

    Fix cart API and update backend

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const products = require("../data/recommented.json");
+const products = require("../data/recommendation.json");
 
 router.get("/recommendation", (req, res) => {
   res.status(200).json({
