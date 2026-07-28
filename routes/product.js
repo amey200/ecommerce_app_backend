9e536c869cbf231d49f20e34e02aadcca315e13f
amey2027 <amey.vagare03.gmail.com>
2026-07-28 19:10:46 +0530

    Fix cart API and update backend

--- routes/product.js
+++ routes/product.js
@@ -24,13 +24,9 @@ router.get("/get/:category/all", (req, res) => {
 
     const category = req.params.category;
 
-    const filteredProducts = products.data
-    .filter(
+    const filteredProducts = products.data.filter(
         (item) =>
             item.category.toLowerCase() === category.toLowerCase()
-    )
-    .map(
-        (product) => getProductWithFav(product, userEmail)
     );
 
     res.status(200).json({
@@ -48,7 +44,7 @@ router.get("/get/:category/all", (req, res) => {
 
     const category = req.params.category;
     const userEmail = req.query.userEmail;
-
+        
     const filteredProducts = products.data
         .filter(
             (item) =>
